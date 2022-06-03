3af115842bbd3a0d28320bc10143ef9cf03905b0
Antigen-1 <official@antigen.top>
2022-06-03 18:18:19 +0800

    update

--- mian.rkt
+++ mian.rkt
@@ -1,11 +1,20 @@
 #lang racket/base
 (require slideshow/base pict)
-(provide mkDiagram)
+(provide mkDiagram current-aspect mkOutline)
+
+(define current-aspect (make-parameter #f))
 
 (define mkDiagram
-  (lambda (#:title [title #f] #:aspect [aspect #f] #:layout [layout 'auto] #:para [para #f] #:line [line #t] #:pict pict #:reference reference)
-    (slide #:title title #:aspect aspect #:layout layout
+  (lambda (#:title [title #f] #:layout [layout 'auto] #:para [para #f] #:line [line #t] #:pict pict #:reference reference)
+    (slide #:title title #:aspect (current-aspect) #:layout layout
            (if para para 'nothing)
            (if line (hline (current-para-width) (current-gap-size)) 'nothing)
            pict
            reference)))
+
+(define mkOutline
+  (lambda (#:title [title #f] #:layout [layout 'auto] #:line [line #t] . items)
+    (slide
+     #:title title #:layout layout
+     (if line (hline (current-para-width) (current-gap-size)) 'nothing)
+     (apply item items))))
